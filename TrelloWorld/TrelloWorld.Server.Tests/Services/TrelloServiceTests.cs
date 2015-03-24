91957d424867d68ebcac28d0f0ca87516ecc012b
Josh Quintus <josh@dotalign.com>
2015-03-23 21:11:56 -0400

    Initial commit

---
+++ TrelloWorld/TrelloWorld.Server.Tests/Services/TrelloServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using TrelloNet;
+using TrelloWorld.Server.Services;
+
+namespace TrelloWorld.Server.Tests.Services
+{
+    [TestFixture]
+    public class TrelloServiceTests
+    {
+        private Mock<IAsyncTrello> _trelloMock;
+        private TrelloService _service;
+
+        [Test]
+        public async Task AddComment_RawCommentIsNull_DoesNothing()
+        {
+            // Act
+            await _service.AddComment(null);
+
+            // Assert
+            _trelloMock.Verify(t => t.Cards.AddComment(It.IsAny<ICardId>(), It.IsAny<string>()), Times.Never);
+
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _trelloMock = new Mock<IAsyncTrello>();
+            _service = new TrelloService(_trelloMock.Object);
+        }
+    }
+}
---
+++ TrelloWorld/TrelloWorld.Server/Services/TrelloService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using TrelloNet;
+
+namespace TrelloWorld.Server.Services
+{
+    public class TrelloService
+    {
+        private readonly IAsyncTrello _trello;
+
+        public TrelloService(IAsyncTrello trello)
+        {
+            if (trello == null) throw new ArgumentNullException();
+            _trello = trello;
+        }
+
+        public async Task AddComment(string rawComment)
+        {
+            string cardId = ParseId(rawComment);
+            if (string.IsNullOrWhiteSpace(cardId)) return;
+
+            var card = await _trello.Cards.WithId(cardId);
+
+            if (card == null) return;
+            await _trello.Cards.AddComment(card, rawComment);
+        }
+
+        private string ParseId(string rawComment)
+        {
+            return null;
+        }
+
+    }
+}
