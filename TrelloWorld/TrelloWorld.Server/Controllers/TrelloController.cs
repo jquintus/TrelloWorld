0c8e8feb00779e8257dce064840ca0b4f1363287
Josh Quintus <josh@dotalign.com>
2015-03-23 21:11:42 -0400

    Initial commit

---
+++ TrelloWorld/TrelloWorld.Server/Controllers/TrelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TrelloWorld.Server.Controllers
+{
+    public class TrelloController : ApiController
+    {
+
+        // POST: api/Trello
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Trello/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+    }
+}
---
+++ TrelloWorld/TrelloWorld.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrelloWorld.Server.WebApiApplication" Language="C#" %>
