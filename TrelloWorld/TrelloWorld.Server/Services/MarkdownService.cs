859b0dbee6a5d95fcb43bf9597a57936245557cf
Josh Quintus <josh@dotalign.com>
2015-04-03 02:48:56 -0400

    make an interface for this

--- TrelloWorld/TrelloWorld.Server/Services/MarkdownService.cs
+++ TrelloWorld/TrelloWorld.Server/Services/MarkdownService.cs
@@ -4,27 +4,33 @@ using System.Threading.Tasks;
 
 namespace TrelloWorld.Server.Services
 {
-    public class MarkdownService
+    public interface IMarkdownService
     {
+        Task<string> GetConfigureAppKey();
+    }
+
+    public class MarkdownService : IMarkdownService
+    {
+        private readonly Markdown _md;
         private readonly string _rootPath;
 
         public MarkdownService(string rootPath)
         {
             _rootPath = rootPath;
+            _md = new Markdown();
         }
+
         public async Task<string> GetConfigureAppKey()
         {
             return await GetMarkdown(Path.Combine(_rootPath, "ConfigureAppKey.md"));
         }
 
-        private static async Task<string> GetMarkdown(string fileName)
+        private async Task<string> GetMarkdown(string fileName)
         {
-            Markdown md = new Markdown();
-
             using (var reader = new StreamReader(fileName))
             {
                 var raw = await reader.ReadToEndAsync();
-                return md.Transform(raw);
+                return _md.Transform(raw);
             }
         }
     }
