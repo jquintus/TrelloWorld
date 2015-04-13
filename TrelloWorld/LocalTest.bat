18b022b2ab614a3d363a5aa94dd9d5d112b39910
Josh Quintus <josh@dotalign.com>
2015-04-13 10:57:38 -0400

    Add some useful output when running test scripts

--- TrelloWorld/LocalTest.bat
+++ TrelloWorld/LocalTest.bat
@@ -1,1 +1,4 @@
+@echo Sending: 
+@cat SampleCommit.json
+
 curl localhost:8696 -X POST -d @SampleCommit.json -H "Content-Type: application/json"
--- TrelloWorld/RemoteTest.bat
+++ TrelloWorld/RemoteTest.bat
@@ -1,1 +1,4 @@
+@echo Sending: 
+@cat SampleCommit.json
+
 curl trelloworld.azurewebsites.net -X POST -d @SampleCommit.json -H "Content-Type: application/json"
